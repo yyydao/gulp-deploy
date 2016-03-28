8151fd27681d9d898f0619ee3b70a6536bd14a8a
yyy <yangyingyu@PC-YANGYINGYU.ppmoney.com>
2016-03-28 14:59:37 +0800

    整理更新

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-#Gulp deploy tool
+# Gulp deploy tool
 
 use Gulp 4.0 and ES2015
+
+# 使用指南
+
+```
+// 常用命令
+-n 参数表示要处理的文件夹
+
+// 带sourcemap的sass自动编译
+gulp sass-dev -n example_dir
+
+// 压缩sass生成的css文件
+gulp sass-min -n example_dir
+
+// 开发用watch&reload,默认端口3000
+gulp watch-all -n example_dir
+
+// 发布到CDN，带md5的非覆盖式发布
+gulp deploy:cdn -n example_dir
+
+// 发布到CDN，覆盖更新
+gulp deploy:cdn-norev -n example_dir
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const less = require('gulp-less');
-const sass = require('gulp-sass');
-const prefix = require('gulp-autoprefixer');
-const cssmin = require('gulp-cssmin');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync');
-const rename = require('gulp-rename');
-const RevAll = require('gulp-rev-all');
-const mime = require('mime-types');
-const path = require('path');
-const Filter = require('gulp-filter');
-const debug = require('gulp-debug');
-const glob = require('glob');
-const revRelace = require('gulp-rev-replace');
-const ngAnnotate = require('gulp-ng-annotate');
+const gulp = require('gulp')
+const less = require('gulp-less')
+const sass = require('gulp-sass')
+const prefix = require('gulp-autoprefixer')
+const cssmin = require('gulp-cssmin')
+const uglify = require('gulp-uglify')
+const sourcemaps = require('gulp-sourcemaps')
+const browserSync = require('browser-sync')
+const rename = require('gulp-rename')
+const RevAll = require('gulp-rev-all')
+const mime = require('mime-types')
+const path = require('path')
+const Filter = require('gulp-filter')
+const debug = require('gulp-debug')
+const glob = require('glob')
+const revRelace = require('gulp-rev-replace')
+const ngAnnotate = require('gulp-ng-annotate')
 
-const UPYUN = require('upyun');
-const gulpConfig = require('./config/config.json');
-const upyunConfig = gulpConfig.upyun;
+const UPYUN = require('upyun')
+const gulpConfig = require('./config/config.json')
+const upyunConfig = gulpConfig.upyun
 
-const del = require('del');
-const args = require('minimist')(process.argv.slice(2));
+const del = require('del')
+const args = require('minimist')(process.argv.slice(2))
 // 0:node,1:文件路径
-const reload = browserSync.reload;
+const reload = browserSync.reload
 
 // jsp文件夹
-let targetPath;
+let targetPath
 
 /**
  1.-n : 构建项目名称，必须带上
@@ -36,21 +36,21 @@ let targetPath;
  */
 // 1.获取项目名称
 if (!args.n) {
-  console.log('请使用 -n 参数设置项目名称， 比如 -n view/0818/web');
-  process.exit(0);
+  console.log('请使用 -n 参数设置项目名称， 比如 -n view/0818/web')
+  process.exit(0)
 }
 
-//需要修改资源引用的文件夹
+// 需要修改资源引用的文件夹
 if (args.t) {
-  targetPath = args.t.toString();
+  targetPath = args.t.toString()
 }
 // 2. 是否进行构建
 // （当启动服务器的时候是在项目工程启动还是构建目录启动）
-if (args.dist) args.domain = false;
+if (args.dist) args.domain = false
 
 // 3.控制是否线上发布
 // 获取线上版本参数
-const projectName = args.n.toString();
+const projectName = args.n.toString()
 const cdn = gulpConfig.production_cdn.domain
 
 // 4.获取路径
@@ -58,37 +58,36 @@ const cdn = gulpConfig.production_cdn.domain
 const paths = {
   dir: path.resolve(__dirname),
   dist: path.resolve(__dirname, 'dist', projectName),
-  jsp: path.resolve(__dirname, 'dist', projectName, 'jsp'),
-};
+  jsp: path.resolve(__dirname, 'dist', projectName, 'jsp')
+}
 
-const projPath = path.resolve(paths.dir, projectName);
-const projectBase = projPath;
+const projPath = path.resolve(paths.dir, projectName)
+const projectBase = projPath
 
+gulp.task('clean', () => del([ paths.dist ]))
 
-gulp.task('clean', () => del([paths.dist]));
-
-function browserSyncFn() {
+function browserSyncFn () {
   browserSync({
     server: {
-      baseDir: projectBase,
+      baseDir: projectBase
     }
-  });
+  })
 }
 
 gulp.task('browser-sync', () => {
-  browserSync.init(['css/*.css', '*.js'], {
+  browserSync.init([ 'css/*.css', '*.js' ], {
     server: { baseDir: projectBase },
-    port: 80,
-  });
-});
+    port: 80
+  })
+})
 
 // LESS
 gulp.task('less', () => {
-  return gulp.src(['./' + projectBase + '/less/shop.less'])
+  return gulp.src([ './' + projectBase + '/less/shop.less' ])
     .pipe(less())
     .pipe(gulp.dest('./' + projectBase + '/css/'))
-    .pipe(browserSync.reload({ stream: true }));
-});
+    .pipe(browserSync.reload({ stream: true }))
+})
 
 // SASS
 
@@ -99,112 +98,110 @@ gulp.task('sass-dev', () => {
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest(projectBase + '/css/'));
-});
+    .pipe(gulp.dest(projectBase + '/css/'))
+})
 
 // 无sourcemap的sass上线用编译
 gulp.task('sass-prod', () => {
   return gulp.src(projectBase + '/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest(projectBase + '/css/'));
-});
+    .pipe(gulp.dest(projectBase + '/css/'))
+})
 
 // 压缩sass生成的css文件
-gulp.task('sass-min', ()=> {
+gulp.task('sass-min', () => {
   return gulp.src(projectBase + '/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
     .pipe(rename({
-      suffix: '.min',
+      suffix: '.min'
     }))
-    .pipe(gulp.dest(projectBase + '/css/'));
-});
+    .pipe(gulp.dest(projectBase + '/css/'))
+})
 
 // 压缩js  @todo:refactor with filter module
 gulp.task('compressjs', () => {
   const compressjsConfig = {
-    mangle: { except: ['$', 'define', 'require', 'exports'] },
-    compress: false,
-  };
-  return gulp.src([projPath + '/**/*.{js}'])
+    mangle: { except: [ '$', 'define', 'require', 'exports' ] },
+    compress: false
+  }
+  return gulp.src([ projPath + '/**/*.{js}' ])
     .on('end', () => {
-      console.log('读取JS文件路径 \n' + projectBase);
+      console.log('读取JS文件路径 \n' + projectBase)
     })
     .pipe(uglify(compressjsConfig))
 
     .pipe(rename({
-      extname: '_min.js',
+      extname: '_min.js'
     }))
     .pipe(gulp.dest(projectBase + '/js'))
     .on('end', () => {
-      console.log('JS压缩完成，输出到 \n' + projectBase + '/js');
-    });
-});
+      console.log('JS压缩完成，输出到 \n' + projectBase + '/js')
+    })
+})
 
 // 上线后debug   @todo:refactor with filter module
 gulp.task('watch-debug', () => {
-  gulp.series('sass-dev', 'compressjs');
-  gulp.watch(['scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js'], { cwd: projectBase }, () => {
-    gulp.series('sass-dev', 'compressjs');
-  });
-});
+  gulp.series('sass-dev', 'compressjs')
+  gulp.watch([ 'scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js' ], { cwd: projectBase }, () => {
+    gulp.series('sass-dev', 'compressjs')
+  })
+})
 
 // 上线后debug
 gulp.task('watch-debug-min', () => {
-  gulp.parallel('sass-min', 'compressjs');
-  gulp.watch(['scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js'], { cwd: projectBase }, () => {
-    gulp.parallel('sass-min', 'compressjs');
-  });
-});
+  gulp.parallel('sass-min', 'compressjs')
+  gulp.watch([ 'scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js' ], { cwd: projectBase }, () => {
+    gulp.parallel('sass-min', 'compressjs')
+  })
+})
 
 gulp.task('clean', () => {
-  return del([paths.dist]);
-});
-
+  return del([ paths.dist ])
+})
 
 // sass开发用watch,默认端口3000
-gulp.task('watch-all', gulp.series('sass-dev', ()=> {
-    browserSyncFn();
-    gulp.watch(['scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js','js/**/*.js', '*.html','**/**/*.html','**/*','!.idea/*','!node_modules/*','!css/**/*.css','!css/**/*.scss'],
-      { cwd: projectBase },
-      gulp.series('sass-dev', ()=>reload())
-    );
-  })
-);
+gulp.task('watch-all', gulp.series('sass-dev', () => {
+  browserSyncFn()
+  gulp.watch([ 'scss/**/*.scss', 'scss/**/*.sass', 'js/**/*.js', 'js/**/*.js', '*.html', '**/**/*.html', '**/*', '!.idea/*', '!node_modules/*', '!css/**/*.css', '!css/**/*.scss' ],
+    { cwd: projectBase },
+    gulp.series('sass-dev', () => reload())
+  )
+}))
 
 gulp.task('build', gulp.series('clean', () => {
   const revAll = new RevAll({
     // html,.min.js,.min.css不加md5
-    dontRenameFile: ['.html', '.min.js', '.min.css', /angular\/.*/g],
-    dontUpdateReference: ['.html', /angular\/.*/g, '.min.css'],
-    transformPath(rev, source, file) {
+    dontRenameFile: [ '.html', '.min.js', '.min.css', /angular\/.*/g ],
+    dontUpdateReference: [ '.html', /angular\/.*/g, '.min.css' ],
+    transformPath (rev, source, file) {
       /* rev - revisioned reference path  调整后的路径
        source - original reference path  源路径(相对于html的路径)
        path - path to the file         文件路径(绝对路径)*/
 
       // 如果是cdn绝对地址，不做转换
-      if (file.path.indexOf('//') === 0) return;
+      if (file.path.indexOf('//') === 0) return
 
       // 如果不是发布到dist目录,静态资源+cdn前缀
       if (args.domain !== false) {
-        let filePath = file.path.slice(__dirname.lastIndexOf('/'));
-        return cdn + filePath;
+        let filePath = file.path.slice(__dirname.lastIndexOf('/'))
+        return cdn + filePath
       }
       // 如果发布到dist目录，返回转化后地址
-      return rev;
+      return rev
     }
 
-  });
+  })
 
   // css，html,js筛选
-  const cssFilter = Filter(['**/*.css'], { restore: true });
-  const htmlFilter = Filter('**/*.html', { restore: true });
-  const jsFilter = Filter(['**/*.js', '!js/angular/**'], { restore: true });
-  const angularFilter = Filter(['**/*.js', 'js/angular/**'], { restore: true });
+  const cssFilter = Filter([ '**/*.css' ], { restore: true })
+  /* const htmlFilter = Filter('**!/!*.html', { restore: true })*/
+  const jsFilter = Filter([ '**/*.js', '!js/angular/**' ], { restore: true })
+  const angularFilter = Filter([ '**/*.js', 'js/angular/**' ], { restore: true })
 
-  return gulp.src([projPath + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}'])
+  return gulp.src([ projPath + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}' ])
     .pipe(revAll.revision())
     .pipe(cssFilter)
     .pipe(cssmin())
@@ -223,18 +220,17 @@ gulp.task('build', gulp.series('clean', () => {
     .pipe(angularFilter.restore)
     .pipe(gulp.dest(paths.dist))
     .pipe(revAll.manifestFile())
-    .pipe(gulp.dest(paths.dist));
-}));
+    .pipe(gulp.dest(paths.dist))
+}))
 
-gulp.task('build-norev', gulp.series('clean',  () => {
-
+gulp.task('build-norev', gulp.series('clean', () => {
   // css，html,js筛选
-  const cssFilter = Filter(['**/*.css'], { restore: true });
-  const htmlFilter = Filter('**/*.html', { restore: true });
-  const jsFilter = Filter(['**/*.js', '!js/angular/**'], { restore: true });
-  const angularFilter = Filter(['**/*.js', 'js/angular/**'], { restore: true });
+  const cssFilter = Filter([ '**/*.css' ], { restore: true })
+  /*  const htmlFilter = Filter('**!/!*.html', { restore: true })*/
+  const jsFilter = Filter([ '**/*.js', '!js/angular/**' ], { restore: true })
+  const angularFilter = Filter([ '**/*.js', 'js/angular/**' ], { restore: true })
 
-  return gulp.src([projPath + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}'])
+  return gulp.src([ projPath + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}' ])
     .pipe(cssFilter)
     .pipe(cssmin())
     .pipe(cssFilter.restore)
@@ -251,72 +247,69 @@ gulp.task('build-norev', gulp.series('clean',  () => {
     .pipe(uglify())
     .pipe(angularFilter.restore)
     .pipe(gulp.dest(paths.dist))
-}));
+}))
 
-gulp.task('jsp-publish', gulp.series('build', ()=> {
-  const manifest = gulp.src(paths.dist + '/rev-manifest.json');
-  return gulp.src([targetPath + '/**/*.jsp'])
-    .pipe(revRelace({ replaceInExtensions: ['.jsp'], manifest: manifest }))
-    .pipe(gulp.dest(paths.jsp));
-}));
+gulp.task('jsp-publish', gulp.series('build', () => {
+  const manifest = gulp.src(paths.dist + '/rev-manifest.json')
+  return gulp.src([ targetPath + '/**/*.jsp' ])
+    .pipe(revRelace({ replaceInExtensions: [ '.jsp' ], manifest: manifest }))
+    .pipe(gulp.dest(paths.jsp))
+}))
 
-gulp.task('deploy:cdn', gulp.series('clean', 'build', function upload(cb) {
-    const upyun = new UPYUN(upyunConfig.bucket, upyunConfig.operator, upyunConfig.password, 'v0');
-    const localFile = glob.sync(paths.dist + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}'); // @todo:考虑到有css、js、html、img以外的文件，应修改成不上传某些东西*/;
+// 发布到CDN，带md5的非覆盖式发布
+gulp.task('deploy:cdn', gulp.series('clean', 'build', function upload (cb) {
+  const upyun = new UPYUN(upyunConfig.bucket, upyunConfig.operator, upyunConfig.password, 'v0')
+  const localFile = glob.sync(paths.dist + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}') // @todo:考虑到有css、js、html、img以外的文件，应修改成不上传某些东西*/
 
-    let i = 0;
+  let i = 0
 
-    for(var file of localFile){
+  for (var file of localFile) {
+    let uploadFile = file.replace(/.:\/(.+\/)*!/, '')
+    let realFileName = file.replace(glob.sync(paths.dist), '').replace(uploadFile, '').replace(/\/$/, '')
+    let remotePath = '/' + projectName + realFileName
 
-      let uploadFile = file.replace(/.:\/(.+\/)*!/, '');
-      let realFileName = file.replace(glob.sync(paths.dist), '').replace(uploadFile, '').replace(/\/$/, '');
-      let remotePath = '/' + projectName + realFileName;
+    upyun.uploadFile(remotePath, file, mime.lookup(file), true, (err, result) => {
+      if (err) console.log(err)
+      if (result.statusCode !== 200) {
+        console.log('好像出问题了')
+        console.log(result)
+      }
+      i++
+      if (i === localFile.length) {
+        console.log(i + '个资源文件被上传到又拍云CDN')
+        cb()
+      }
+    })
+  }
+}))
 
-      upyun.uploadFile(remotePath, file, mime.lookup(file), true, (err, result) => {
-        if (err) console.log(err);
-        if (result.statusCode !== 200) {
-          console.log('好像出问题了');
-          console.log(result);
-        }
-        i++;
-        if (i === localFile.length) {
-          console.log(i + '个资源文件被上传到又拍云CDN');
-          cb();
-        }
-      });
-    }
+// 发布到CDN，覆盖更新
+gulp.task('deploy:cdn-norev', gulp.series('clean', 'build-norev', function upload (cb) {
+  const upyun = new UPYUN(upyunConfig.bucket, upyunConfig.operator, upyunConfig.password, 'v0')
+  const localFile = glob.sync(paths.dist + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}') // @todo:考虑到有css、js、html、img以外的文件，应修改成不上传某些东西*/
 
-  })
-);
+  let i = 0
 
-gulp.task('deploy:cdn-norev', gulp.series('clean', 'build-norev', function upload(cb) {
-    const upyun = new UPYUN(upyunConfig.bucket, upyunConfig.operator, upyunConfig.password, 'v0');
-    const localFile = glob.sync(paths.dist + '/**/*.{png,jpg,gif,html,css,js,eot,svg,ttf,woff,woff2}'); // @todo:考虑到有css、js、html、img以外的文件，应修改成不上传某些东西*/;
+  for (var file of localFile) {
+    let uploadFile = file.replace(/.:\/(.+\/)*!/, '')
+    let realFileName = file.replace(glob.sync(paths.dist), '').replace(uploadFile, '').replace(/\/$/, '')
+    let remotePath = '/' + projectName + realFileName
 
-    let i = 0;
+    upyun.uploadFile(remotePath, file, mime.lookup(file), true, (err, result) => {
+      if (err) console.log(err)
+      if (result.statusCode !== 200) {
+        console.log('好像出问题了')
+        console.log(result)
+      }
+      i++
+      if (i === localFile.length) {
+        console.log(i + '个资源文件被上传到又拍云CDN')
+        cb()
+      }
+    })
+  }
+}))
 
-    for(var file of localFile){
-      let uploadFile = file.replace(/.:\/(.+\/)*!/, '');
-      let realFileName = file.replace(glob.sync(paths.dist), '').replace(uploadFile, '').replace(/\/$/, '');
-      let remotePath = '/' + projectName + realFileName;
+gulp.task('sass-publish', gulp.parallel('sass-prod', 'sass-min'))
 
-      upyun.uploadFile(remotePath, file, mime.lookup(file), true, (err, result) => {
-        if (err) console.log(err);
-        if (result.statusCode !== 200) {
-          console.log('好像出问题了');
-          console.log(result);
-        }
-        i++;
-        if (i === localFile.length) {
-          console.log(i + '个资源文件被上传到又拍云CDN');
-          cb();
-        }
-      });
-    }
-  })
-);
-
-gulp.task('sass-publish', gulp.parallel('sass-prod', 'sass-min'));
-
-gulp.task('default', gulp.series('sass-dev', 'watch-all', 'browser-sync'));
- 
+gulp.task('default', gulp.series('sass-dev', 'watch-all', 'browser-sync'))
